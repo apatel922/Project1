ae7d4ce50df144fc48b33cd7dfa305387706cddf
luke williams <jluke94williams@gmail.com>
2020-03-02 20:57:54 -0500

    Integrated virus tracker Api into Earth

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -23,23 +23,51 @@ $(document).ready(function (){
         console.log(response1)
     })
 
+
+    todaysDate = moment().format('L');
+
+
     function initialize() {
-        var earth = new WE.map('earth_div');
-        WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
 
-        var europe = WE.marker([45, 11]).addTo(earth);
-        europe.bindPopup("<h1>Europe</h1>", {maxWidth: 150, closeButton: true}).openPopup();
+        var trackerQueryURL = "https://coronavirus-tracker-api.herokuapp.com/all"
 
-        var africa = WE.marker([8, 20]).addTo(earth);
-        africa.bindPopup("<h1>Africa</h1>", {maxWidth: 150, closeButton: true}).openPopup();
+        $.ajax({
+            url: trackerQueryURL,
+            method: "GET"
+        }).then(function (response){
+            console.log(response)
 
-        var asia = WE.marker([33, 103]).addTo(earth);
-        asia.bindPopup("<h1>Asia</h1>", {maxWidth: 120, closeButton: true}).openPopup();
+            var chinaCases = 0;
+            for (var i = 0; i < 30; i++ ) {
+                chinaCases+=(response.confirmed.locations[i].latest);
+            }
+            console.log(chinaCases)
 
-        var northAmerica = WE.marker([35, -100]).addTo(earth);
-        northAmerica.bindPopup("<h1>North America</h1>", {maxWidth: 120, closeButton: true}).openPopup();
+        
+            var earth = new WE.map('earth_div');
+            WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
+    
+            var europe = WE.marker([45, 11]).addTo(earth);
+            europe.bindPopup("<h1>Europe</h1><ul><li></li></ul>", {maxWidth: 150, closeButton: true}).openPopup();
+    
+            var africa = WE.marker([8, 20]).addTo(earth);
+            africa.bindPopup("<h1>Africa</h1>", {maxWidth: 150, closeButton: true}).openPopup();
+    
+            var china = WE.marker([33, 103]).addTo(earth);
+            china.bindPopup("<h1>China</h1><ul><li>Confirmed Cases (as of " + todaysDate + "): "  + chinaCases + "</li></ul>", {maxWidth: 120, closeButton: true}).openPopup();
+    
+            var northAmerica = WE.marker([35, -100]).addTo(earth);
+            northAmerica.bindPopup("<h1>North America</h1>", {maxWidth: 120, closeButton: true}).openPopup();
+    
+            earth.setView([0, 0], 3.5);
+        
+        
+        
+        })
 
-        Earch.setView([0, 0], 100);
+
+
+        
     }
 
     initialize()
